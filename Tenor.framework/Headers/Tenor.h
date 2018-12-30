0267de9d79e4518b560636e049a5009bc907281f
Shraboni <torongo.ruet@yahoo.com>
2018-12-30 14:55:13 +0600

    FrameworkUploaded

---
+++ Tenor.framework/Headers/Tenor.h
@@ -0,0 +1,19 @@
+//
+//  Tenor.h
+//  Tenor
+//
+//  Created by Inventive Apps on 25/12/18.
+//  Copyright © 2018 Nagib Bin Azad. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Tenor.
+FOUNDATION_EXPORT double TenorVersionNumber;
+
+//! Project version string for Tenor.
+FOUNDATION_EXPORT const unsigned char TenorVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Tenor/PublicHeader.h>
+
+
